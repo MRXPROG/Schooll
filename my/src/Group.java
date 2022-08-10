72f3562fdfb2758ee8ee019508fd7fc8f9a3d525
Artem Popov <br4v0.2403@gmail.com>
2022-08-10 17:53:41 +0300

    GG

--- my/src/Group.java
+++ my/src/Group.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
 
 public class Group {
-    List<Student> Grouplist = new ArrayList(Arrays.asList(
-            Student student1 = new Student("Artem", "Vietnam", "Group - 21 - 9", 12),
-            Student student1 = new Student("Den", "Dunchik", "Group - 21 - 9", 22)
-    ));
+    List<Student> Grouplist;
+
+    public addMember(Student student)
+    {
+        Grouplist.add(student);
+    }
 
     public Group(){
         group = new ArrayList<Pupil>();
