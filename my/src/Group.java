b9c65de57ce44dcdd4e994aeb110a817ca30882e
Denys Postolnyi <postlden44@gmail.com>
2022-08-10 17:53:22 +0300

    erg

--- my/src/Group.java
+++ my/src/Group.java
@@ -1,17 +1,12 @@
 import java.util.ArrayList;
 
 public class Group {
-<<<<<<< HEAD
     ArrayList<Pupil> group;
-=======
     List<Student> Grouplist = new ArrayList(Arrays.asList(
             Student student1 = new Student("Artem", "Vietnam", "Group - 21 - 9", 12),
             Student student1 = new Student("Den", "Dunchik", "Group - 21 - 9", 22)
     ));
-
->>>>>>> origin/main
     public Group(){
         group = new ArrayList<Pupil>();
     }
-
 }
--- my/src/School.java
+++ my/src/School.java
@@ -4,18 +4,10 @@ public class School {
     ArrayList<Group> classes;
 
     public School() {
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/main
         classes = new ArrayList<Classes>();
     }
 
     public void addClass(Group group) {
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/main
         this.classes.add(group);
     }
 }
