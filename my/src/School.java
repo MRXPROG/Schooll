9e748b3a9fd6d1df8c95fba0b1187edd8d1b1955
Denys Postolnyi <postlden44@gmail.com>
2022-08-10 17:16:08 +0300

    groups

---
+++ my/src/School.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class School {
+    ArrayList<Group> classes;
+
+    public School() {
+        classes = new ArrayList<Classes>();
+    }
+
+    public void addClass(Group group) {
+        this.classes.add(group);
+    }
+}
